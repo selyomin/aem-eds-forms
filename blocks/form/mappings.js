9eeadb5a0bbaab9f3748d826951dc0602c7201ff
Andrei_Selyomin <andrei_selyomin@epam.com>
2025-10-05 13:55:53 -0400

    add new country-dropdown component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['range'];
+let customComponents = ['country-dropdown', 'range'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
